8e3d15087f7541ef1983d4ffc3f49f89ced8d1e4
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-06-19 08:50:36 -0400

    done

--- ruby/reverse_string.rb
+++ ruby/reverse_string.rb
@@ -1,6 +1,24 @@
+require 'pry'
 def reverse_string(str)
   # type your code in here
-end
+
+  # reversedString = ''
+  # string_length = str.length - 1
+  # string_length.downto(0).each do |l|
+  #   reversedString << str[l]
+  # end
+  # reversedString
+
+  str = str.split("")
+  reversed = []
+  str.each {|i| reversed.unshift(i)}
+  reversed.join("")
+
+
+
+
+
+
 
 if __FILE__ == $PROGRAM_NAME
   puts "Expecting: 'ih'"
