0330666ec54ebe0423f739bef599d922ebf2d1b8
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-08-19 15:56:49 -0400

    javascript

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,6 +1,21 @@
+let str = "hi my name is nicholas"
+
 function reverseString(str) {
   // type your code here
+  let arr = str.split('')
+  let reversed = []
+  for (let i = arr.length; i >0; i --){
+    reversed.push(arr[i])
+  }
+return reversed.join("")
+ 
+  // return reversed
+  // .join("")
 }
+
+reverseString('hi')
+
+
 
 if (require.main === module) {
   // add your own tests in here
@@ -17,3 +32,8 @@ module.exports = reverseString;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+//create a new blank string
+//use a for loop iterating over the letters of the original string, starting from
+//the end and decreimenting, putting each letter into the new string in reverse
+//order
